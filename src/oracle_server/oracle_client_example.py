c67a6526b7b51912796d96378e5b7de4d7a4486a
Philipp Guevorguian <pguevorg@uwo.ca>
2024-05-09 18:24:51 +0400

    include the dgx url in oracle client call example

--- src/oracle_server/oracle_client_example.py
+++ src/oracle_server/oracle_client_example.py
@@ -8,17 +8,18 @@ def score_with_oracle(input_strings,oracle_url):
     return dict(response.json())
 
 if __name__ == '__main__':
-    oracle_url =  'http://ap.yerevann.com:5006/oracles/vina/drd2'
-    input_strings = ["CCO","CCCCN"]
+    oracle_url = 'http://172.26.26.101:5006/oracles/vina/drd2'
+    #oracle_url =  'http://ap.yerevann.com:5006/oracles/vina/drd2'
+    input_strings = ["CCO"]
     print(score_with_oracle(input_strings,oracle_url))
-    oracle_url =  'http://ap.yerevann.com:5006/oracles/vina/ache'
-    print(score_with_oracle(input_strings,oracle_url))
-    oracle_url =  'http://ap.yerevann.com:5006/oracles/vina/mk2'
-    print(score_with_oracle(input_strings,oracle_url))
-    oracle_url =  'http://ap.yerevann.com:5006/oracles/tdc/drd2'
-    print(score_with_oracle(input_strings,oracle_url))
-    oracle_url =  'http://ap.yerevann.com:5006/oracles/tdc/gsk3b'
-    print(score_with_oracle(input_strings,oracle_url))
-    input_strings = "CCCCCCC"
-    oracle_url =  'http://ap.yerevann.com:5006/oracles/tdc/jnk3'
-    print(score_with_oracle(input_strings,oracle_url))
+    # oracle_url =  'http://ap.yerevann.com:5006/oracles/vina/ache'
+    # print(score_with_oracle(input_strings,oracle_url))
+    # oracle_url =  'http://ap.yerevann.com:5006/oracles/vina/mk2'
+    # print(score_with_oracle(input_strings,oracle_url))
+    # oracle_url =  'http://ap.yerevann.com:5006/oracles/tdc/drd2'
+    # print(score_with_oracle(input_strings,oracle_url))
+    # oracle_url =  'http://ap.yerevann.com:5006/oracles/tdc/gsk3b'
+    # print(score_with_oracle(input_strings,oracle_url))
+    # input_strings = "CCCCCCC"
+    # oracle_url =  'http://ap.yerevann.com:5006/oracles/tdc/jnk3'
+    # print(score_with_oracle(input_strings,oracle_url))
