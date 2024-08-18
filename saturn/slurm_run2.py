34899d2a85b21c7fb5292735d004c12eca44d747
tigranfah <tigran.fahradyan01@gmail.com>
2024-08-18 19:03:02 +0400

    update the config files, add submitit code for all models.

--- saturn/slurm_run2.py
+++ saturn/slurm_run2.py
@@ -23,13 +23,15 @@ if __name__ == "__main__":
     all_seeds = get_all_seeds()
 
     config_file_path = "chemlactica_125m_hparams.yaml"
+    # config_file_path = "chemlactica_1.3b_hparams.yaml"
+    # config_file_path = "chemma_2b_hparams.yaml"
     default_config = yaml.safe_load(open(config_file_path))
     infer_configs = [default_config]
     model_name = "-".join(config_file_path.split("/")[-1].split("_")[:2])
 
-    executor = submitit.AutoExecutor(folder="slurm_jobs/saturn/job_%j")
+    executor = submitit.AutoExecutor(folder="~/slurm_jobs/saturn/job_%j")
     executor.update_parameters(
-        name="chemlactica-against-saturn", timeout_min=int(len(target_proteins) * len(all_seeds) * 30),
+        name="chemlactica-against-saturn", timeout_min=int(4 * 60),
         gpus_per_node=1,
         nodes=1, mem_gb=30, cpus_per_task=10,
         slurm_array_parallelism=10
@@ -45,7 +47,7 @@ if __name__ == "__main__":
             v = 0
             while os.path.exists(os.path.join(base, f"{name}-{v}")):
                 v += 1
-            output_dir = os.path.join(base, f"{name}-v{v}")
+            output_dir = os.path.join(base, f"{name}-{v}")
             os.makedirs(output_dir, exist_ok=True)
             hparams_path = os.path.join(output_dir, f"hparams.yaml")
             yaml.safe_dump(config, open(hparams_path, "w"))
@@ -53,12 +55,12 @@ if __name__ == "__main__":
             for target in target_proteins:
                 for seed in all_seeds:
                     function = submitit.helpers.CommandFunction([
-                        'python3', 'main.py',
+                        'python3', 'run_exp.py',
                         '--config_default', hparams_path,
                         '--output_dir', output_dir,
                         '--target', target,
                         '--seed', str(seed)
                     ])
                     print(' '.join(function.command))
-                    # job = executor.submit(function)
-                    # jobs.append(job)
+                    job = executor.submit(function)
+                    jobs.append(job)
