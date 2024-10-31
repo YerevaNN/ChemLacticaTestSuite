71cc117ba486f3d2cc6a25eb4e0433f260dee448
MenuaB <menooa.vold@gmail.com>
2024-10-31 16:39:18 +0400

    minor changes

--- src/property_2_Mol_config.py
+++ src/property_2_Mol_config.py
@@ -25,11 +25,6 @@ galactica_logits_processors_configs = logits_utils.load_processor_config(galacti
 gemma_lp_configs = [asdict(obj) for obj in gemma_logits_processors_configs]
 galactica_lp_configs = [asdict(obj) for obj in galactica_logits_processors_configs]
 
-
-# target_dists = ["prior", "uniform"]
-# models = [model_2b_32k_8f45, model_2b_3k_504e]
-models = [model_2b_10k_6a86, model_2b_8k_6a86, model_2b_6b_6a86, model_2b_4k_6a86, model_2b_7k_d46c, model_2b_9b_d46c, model_2b_10k_d46c, model_2b_12k_d46c]
-
 evaluation_config = {
     "test_suite":                test_suite,
     "property_range":            property_range,
